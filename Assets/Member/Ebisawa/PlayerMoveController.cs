5dbcef44f2e6efb585648e28c48fac6346135ea7
ebisawashun <shun171219@icloud.com>
2024-07-14 17:34:01 +0900

    プレイヤーの動き

--- Assets/Member/Ebisawa/PlayerMoveController.cs
+++ Assets/Member/Ebisawa/PlayerMoveController.cs
@@ -7,12 +7,12 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rb2d;
     [SerializeField] float speed = 1.0f;
     float x, y;
-    public bool _invincible = false;
+    GameSystemController _gameSystem;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-
+        _gameSystem = FindFirstObjectByType<GameSystemController>();
     }
 
     void Update()
@@ -20,31 +20,35 @@ public class PlayerController : MonoBehaviour
 
         x = Input.GetAxisRaw("Horizontal");
         y = Input.GetAxisRaw("Vertical");
-
     }
     private void FixedUpdate()
     {
+        if (_gameSystem._gameState != GameSystemController.GameState.RunGame)
+        {
+            rb2d.velocity = Vector2.zero;
+            return;
+        }
 
         if (x > 0)
         {
             rb2d.velocity = Vector2.right * speed;
         }
-        if(x < 0)
+        if (x < 0)
         {
             rb2d.velocity = Vector2.left * speed;
         }
-        if(y > 0)
+        if (y > 0)
         {
             rb2d.velocity = Vector2.up * speed;
         }
-        if(y < 0)
+        if (y < 0)
         {
             rb2d.velocity = Vector2.down * speed;
         }
-        //if(x == 0 && y == 0)
-        //{
-        //    rb2d.velocity = Vector2.zero;
-        //}
+        if (x == 0 && y == 0)
+        {
+            rb2d.velocity = Vector2.zero;
+        }
 
     }
 }
