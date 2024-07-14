d32f8dc3b8caf54c5a9c70c17123cb3a1c99cbc0
ebisawashun <shun171219@icloud.com>
2024-07-14 12:25:51 +0900

    プレイヤーの動き

---
+++ Assets/Member/Ebisawa/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    [SerializeField] float speed = 1.0f;
+    float x, y;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+
+    }
+    private void FixedUpdate()
+    {
+
+        if (x > 0)
+        {
+            rb2d.velocity = Vector2.right * speed;
+        }
+        if(x < 0)
+        {
+            rb2d.velocity = Vector2.left * speed;
+        }
+        if(y > 0)
+        {
+            rb2d.velocity = Vector2.up * speed;
+        }
+        if(y < 0)
+        {
+            rb2d.velocity = Vector2.down * speed;
+        }
+        //if(x == 0 && y == 0)
+        //{
+        //    rb2d.velocity = Vector2.zero;
+        //}
+
+    }
+}
