f5db8eb9622dc92161bb5ab61cb21d388e4d49d4
GokiKaneko <gouc4990@gmail.com>
2024-07-14 12:22:15 +0900

    各シーンとシーンチェンジャー

---
+++ Assets/Member/Kaneko/SeneScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneScript : MonoBehaviour
+{
+    private void Awake()
+    {
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
