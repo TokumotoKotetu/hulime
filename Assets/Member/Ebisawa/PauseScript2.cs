106604499c620862c08f517d9d47afc998e4dffd
TokumotoKotetu <k824tokumoto@gmail.com>
2024-07-15 14:37:41 +0900

    バグ修正

--- Assets/Member/Ebisawa/PauseScript2.cs
+++ Assets/Member/Ebisawa/PauseScript2.cs
@@ -1,23 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static GameSystemController;
 
 public class PauseScript2 : MonoBehaviour
 {
     [SerializeField]
     private GameObject pauseUI;
-    
+    GameObject _manager;
+    GameSystemController controller;
 
     // Start is called before the first frame update
     void Start()
     {
         pauseUI.SetActive(false);
+        _manager = GameObject.Find("GameManager");
+        controller = _manager.GetComponent<GameSystemController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Cancel"))
+        if (Input.GetButtonDown("Cancel") && controller._gameState != GameState.Result)
         {
             //　ポーズUIのアクティブ、非アクティブを切り替え
             pauseUI.SetActive(!pauseUI.activeSelf);
--- Assets/Member/Ebisawa/Pausebotton.cs
+++ Assets/Member/Ebisawa/Pausebotton.cs
@@ -1,23 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static GameSystemController;
 
 public class Pausebotton : MonoBehaviour
 {
     [SerializeField] private GameObject pauseUIPrefab;
     private GameObject pauseUIInstance;
-
+	GameObject _manager;
+	GameSystemController controller;
    
     // Start is called before the first frame update
     void Start()
     {
-        
+		_manager = GameObject.Find("GameManager");
+        controller = _manager.GetComponent<GameSystemController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-		if (Input.GetButtonDown ("Cancel")) {
+		if (Input.GetButtonDown ("Cancel") && controller._gameState != GameState.Result) {
 			if (pauseUIInstance == null) {
 				pauseUIInstance = GameObject.Instantiate (pauseUIPrefab) as GameObject;
 				Time.timeScale = 0f;
